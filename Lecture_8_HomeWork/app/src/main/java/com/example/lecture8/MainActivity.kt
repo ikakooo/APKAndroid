8bcea95b06e938d34676727846cdb9a63b6d21d8
unknown <i_chkhitunidze@cu.edu.ge>
2020-04-24 01:23:10 +0400

    fix

--- Lecture_8_HomeWork/app/src/main/java/com/example/lecture8/MainActivity.kt
+++ Lecture_8_HomeWork/app/src/main/java/com/example/lecture8/MainActivity.kt
@@ -8,5 +8,15 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+      
     }
+    private fun save(){
+
+
+
+    }
+
+
+
+
 }
