942e1e55a794fd58a65468395b353bac5166f276
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-04-25 23:00:39 -0700

    second step

--- quiz1/app/src/main/java/com/example/quiz1/MainActivity.kt
+++ quiz1/app/src/main/java/com/example/quiz1/MainActivity.kt
@@ -1,27 +1,44 @@
 package com.example.quiz1
 
+import android.annotation.SuppressLint
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.EditText
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
+    private var userCount = 1
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        init()
-    }
-    private fun init(){
-        interface firstname<K, out V>
-        interface lastname<K, out V>
-        interface age<K, out V>
-        interface email<K, out V>
-       var string: name firsNameText.onFocusChangeListener
-
-
+        addUser()
+        // removeUser()
     }
 
+    @SuppressLint("SetTextI18n")
+    private fun addUser() {
+        eddUserButton.setOnClickListener {
+
+            val userNameSaver = usernameText.text.toString()
+            val ageIntSave = ageInt.text.toString()
+            val emailTextSave = emailText.text.toString()
+            val firstNameSaver = firsNameText.text.toString()
+
+            textViewShow.text =
+                "$userCount ) First Name: $firstNameSaver Last Name: $userNameSaver Age: $ageIntSave Email: $emailTextSave"
+            // textViewShow.text = "ikakooo  $firstNameSaver"
+            userCount++
+        }
+
+    }
+
+    //private fun removeUser(){
+    //   removeUserButton.setOnClickListener{
+
+
+    //   }
+
+    // }
 
 
 }
