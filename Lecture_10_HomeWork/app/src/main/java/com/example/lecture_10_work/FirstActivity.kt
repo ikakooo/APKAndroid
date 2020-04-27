11a29764889f4741452ba408041e6c74588e7bf9
unknown <i_chkhitunidze@cu.edu.ge>
2020-04-28 00:05:27 +0400

    firs commit for 10 homework

---
+++ Lecture_10_HomeWork/app/src/main/java/com/example/lecture_10_work/FirstActivity.kt
@@ -0,0 +1,27 @@
+package com.example.lecture_10_work
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_first.*
+
+class FirstActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_first)
+        init()
+    }
+    private fun init(){
+        firsPageButton.setOnClickListener(){
+            SecondActivity()
+
+        }
+
+    }
+    private fun SecondActivity(){
+        val intent = Intent(this, SecondActivity::class.java)
+        startActivity(intent)
+
+    }
+}
