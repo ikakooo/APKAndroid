3b2bb8baa46c7b12bb814396d90415617237c79d
unknown <i_chkhitunidze@cu.edu.ge>
2020-06-03 09:42:51 +0400

    ewewe

--- Midterm/app/src/main/java/com/example/midterm/activities/notes/LogInNotesActivity.kt
+++ Midterm/app/src/main/java/com/example/midterm/activities/notes/LogInNotesActivity.kt
@@ -21,26 +21,39 @@ class LogInNotesActivity : AppCompatActivity() {
 
 
     private fun init() {
-        LogInButtonID.setOnClickListener(){
-        val email = LogInEmailEditTextsID.text.toString()
-        val password = LogInPasswordEditTextsID.text.toString()
+        LogInButtonID.setOnClickListener() {
+            val email = LogInEmailEditTextsID.text.toString()
+            val password = LogInPasswordEditTextsID.text.toString()
+            if (email.length >= 5 && password.length >= 8) {
+                Toast.makeText(
+                    this,
+                    "please wait! before Email and Password checking",
+                    Toast.LENGTH_LONG
+                ).show()
+                FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
+                    .addOnCompleteListener {
+                        if (!it.isSuccessful) {
+                            Toast.makeText(
+                                this,
+                                "Password or Email doesn't match",
+                                Toast.LENGTH_LONG
+                            ).show()
+                            //return@addOnCompleteListener
+                        } else {
+                            startActivity(Intent(this, LoggedInNotesActivity::class.java))
+                            finish()
+                        }
 
-            Toast.makeText(this, "please wait! before Email and Password checking", Toast.LENGTH_LONG).show()
-            FirebaseAuth.getInstance().signInWithEmailAndPassword(email, password)
-                .addOnCompleteListener {
-                    if (!it.isSuccessful) {
-                        Toast.makeText(this, "Password or Email doesn't match", Toast.LENGTH_LONG).show()
-                        //return@addOnCompleteListener
-                    }else{
-                        startActivity(Intent(this, LoggedInNotesActivity::class.java))
-                        finish()
                     }
-
-                }
+            }else{Toast.makeText(
+                this,
+                "Password or Email doesn't match",
+                Toast.LENGTH_LONG
+            ).show()}
 //                .addOnFailureListener {
 //                    Toast.makeText(this, "Password or Email doesn't match", Toast.LENGTH_LONG).show()
 //                }
-    }
+        }
         RegisterTextViewID.setOnClickListener() {
             startActivity(Intent(this, RegisterInNotesActivity::class.java))
         }
