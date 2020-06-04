2a802d9433f23eeb4e7eec04e621800cd5ffa385
unknown <i_chkhitunidze@cu.edu.ge>
2020-06-05 01:00:52 +0400

    shpashcreeni corutinebit

--- MidtermUPDATE/app/src/main/java/com/example/midterm/activities/SplashScreenActivity.kt
+++ MidtermUPDATE/app/src/main/java/com/example/midterm/activities/SplashScreenActivity.kt
@@ -5,25 +5,29 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.Handler
 import com.example.midterm.R
+import java.lang.Exception
 
 class SplashScreenActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        moveMainActivity()
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(2000)
+                    val intent = Intent(baseContext, WeatherActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+
+        }
+        background.start()
     }
 
-
-
-
-
-    private fun moveMainActivity(){
-        Handler().postDelayed({
-            startActivity(Intent(this, WeatherActivity::class.java))
-            finish()
-        },2000)
-    }
 }
 
 
