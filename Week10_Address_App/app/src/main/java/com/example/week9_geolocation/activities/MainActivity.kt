c7ebb040f94a0eaa1146ca37fb6e46de710ed646
unknown <i_chkhitunidze@cu.edu.ge>
2020-06-18 01:31:28 +0400

    Update

--- Week10_Address_App/app/src/main/java/com/example/week9_geolocation/activities/MainActivity.kt
+++ Week10_Address_App/app/src/main/java/com/example/week9_geolocation/activities/MainActivity.kt
@@ -1,12 +1,24 @@
 package com.example.week9_geolocation.activities
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.example.week9_geolocation.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        init()
+    }
+
+
+    private fun init(){
+        floating_action_button.setOnClickListener {
+            val intent = Intent(this, AddressesSearchActivity::class.java)
+            startActivity(intent)
+        }
+
     }
 }
