683cc9c1f4e2e440de0f17936ca1894c88bfc84d
unknown <i_chkhitunidze@cu.edu.ge>
2020-06-25 02:02:36 +0400

    update

---
+++ ShopApp/app/src/main/java/com/ikakooo/shopapp/SignInActivity.kt
@@ -0,0 +1,84 @@
+package com.ikakooo.shopapp
+
+import android.os.Bundle
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.SpannableStringBuilder
+import android.text.style.ForegroundColorSpan
+import android.util.Log.d
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import kotlinx.android.synthetic.main.activity_sign_in.*
+
+
+class SignInActivity : AppCompatActivity() {
+    private var REMEMBER_ME = 0
+    private var IF_REMEMBER_ME_UNCHEKED = 0
+    private var IF_REMEMBER_ME_CHEKED = 1
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_sign_in)
+        init()
+    }
+
+
+    private fun init() {
+
+
+        setColor(signUpTextViewID, (R.color.colorText), getString(R.string.new_user) + " ")
+        setColor(signUpTextViewID, (R.color.colorAccent), getString(R.string.sign_up) + " ")
+        setColor(signUpTextViewID, (R.color.colorText), getString(R.string.here))
+
+
+
+
+        rememberCheckButtonID.setOnClickListener {
+            if (REMEMBER_ME == IF_REMEMBER_ME_UNCHEKED) {
+                rememberCheckButtonID.setCompoundDrawablesWithIntrinsicBounds(
+                    R.mipmap.cheked,
+                    0,
+                    0,
+                    0
+                )
+                REMEMBER_ME = IF_REMEMBER_ME_CHEKED
+            } else {
+                rememberCheckButtonID.setCompoundDrawablesWithIntrinsicBounds(
+                    R.mipmap.unchecked,
+                    0,
+                    0,
+                    0
+                )
+                REMEMBER_ME = IF_REMEMBER_ME_UNCHEKED
+            }
+
+            d("sdfsdfsdfsdf", REMEMBER_ME.toString())
+        }
+
+
+    }
+
+
+    private fun setColor(view: TextView, color: Int, text: String) {
+        // val builder = SpannableStringBuilder()
+
+
+        val string: Spannable = SpannableString(text)
+        string.setSpan(
+            ForegroundColorSpan(ContextCompat.getColor(this, color)),
+            0,
+            string.length,
+            Spannable.SPAN_EXCLUSIVE_EXCLUSIVE
+        )
+        view.append(string)
+
+
+        //view.setText(builder, TextView.BufferType.SPANNABLE)
+
+    }
+
+
+}
+
+
--- ShopApp/app/src/main/java/com/ikakooo/shopapp/SplashScreenActivity.kt
+++ ShopApp/app/src/main/java/com/ikakooo/shopapp/SplashScreenActivity.kt
@@ -3,25 +3,44 @@ package com.ikakooo.shopapp
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.os.Handler
+import android.util.Log.d
+import java.lang.Exception
 
 class SplashScreenActivity : AppCompatActivity() {
-    var runnable= Runnable {
-        init()
+    private val SplashThread = object : Thread() {
+        override fun run() {
+            try {
+                Thread.sleep(2000)
+                val intent = Intent(baseContext, SignInActivity::class.java)
+                intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
+                startActivity(intent)
+                overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
+                finish()
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_splash_screen)
-        Handler().postDelayed(runnable,2000)
-    }
-    private fun init(){
-
-        val intent= Intent(this,MainActivity::class.java)
-        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
-        startActivity(intent)
-        overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        SplashThread
     }
 
 
+    override fun onResume() {
+        super.onResume()
+        SplashThread.start()
+    }
+
+
+    override fun onPause() {
+        super.onPause()
+        SplashThread.interrupt()
+        d("sdfdfsdfs", SplashThread.isAlive.toString())
+
+    }
 
 }
