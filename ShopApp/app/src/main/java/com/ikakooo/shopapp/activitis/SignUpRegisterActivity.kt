da705fa2082a0e6522e62e8e3034027155fb768d
unknown <i_chkhitunidze@cu.edu.ge>
2020-06-30 00:57:58 +0400

    esff

--- ShopApp/app/src/main/java/com/ikakooo/shopapp/activitis/SignUpRegisterActivity.kt
+++ ShopApp/app/src/main/java/com/ikakooo/shopapp/activitis/SignUpRegisterActivity.kt
@@ -1,17 +1,19 @@
 package com.ikakooo.shopapp.activitis
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import com.ikakooo.shopapp.CustomTools
 import com.ikakooo.shopapp.R
-import kotlinx.android.synthetic.main.activity_sign_in.*
 import kotlinx.android.synthetic.main.activity_sign_up_register.*
+
 
 class SignUpRegisterActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_sign_up_register)
         init()
+        buttons()
     }
 
 
@@ -20,6 +22,16 @@ class SignUpRegisterActivity : AppCompatActivity() {
         CustomTools.setColor(signInTextViewID, (R.color.colorText), "Already a member?" + " ", this)
         CustomTools.setColor(signInTextViewID, (R.color.colorPrimary), " Log in", this)
 
+
+    }
+
+    private fun buttons(){
+        singInButtonID.setOnClickListener(){
+            val intent= Intent(this, CompleteProfileActivity::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK)
+            startActivity(intent)
+            overridePendingTransition(android.R.anim.fade_in,android.R.anim.fade_out)
+        }
 
     }
 }
