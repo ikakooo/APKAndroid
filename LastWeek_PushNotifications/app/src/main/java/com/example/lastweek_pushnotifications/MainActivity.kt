825f1583202a28470c67abf33a3e639610b3001a
ikakooo <i_chkhitunidze@cu.edu.ge>
2020-07-07 23:50:22 +0400

    შეტყობინებები მივიღე ვირტუალურ დივაისზე რამოდენიმე წუთით დაგვიანებით, მაგრამ ჩემს ფიზიკურ დივაისს არ აქვს შესაბამისი გუგლ ფლეი სერვისების მხარდაჭერა და არ მუსაობს

---
+++ LastWeek_PushNotifications/app/src/main/java/com/example/lastweek_pushnotifications/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.lastweek_pushnotifications
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.onesignal.OneSignal
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+
+        // OneSignal Initialization
+        OneSignal.startInit(this)
+                .inFocusDisplaying(OneSignal.OSInFocusDisplayOption.Notification)
+                .unsubscribeWhenNotificationsAreDisabled(true)
+                .init()
+    }
+}
